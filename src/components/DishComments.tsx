6f726063225903ff1ef727b2d0089ee1ec794989
BertoPolo <bertopolo91@gmail.com>
2022-05-19 16:15:08 +0200

    dishcomments solved

--- src/components/DishComments.tsx
+++ src/components/DishComments.tsx
@@ -1,11 +1,26 @@
-import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroup from "react-bootstrap/ListGroup"
 
-const DishComments = (props) => (
+interface selectedDishProps {
+  id: number
+  name: string
+  image: string
+  category: string
+  label: string
+  price: string
+  description: string
+  comments: {
+    id: number
+    rating: number
+    comment: string
+    author: string
+    date: Date
+  }[]
+}
+
+const DishComments = (props: selectedDishProps) => (
   <ListGroup>
-    {props.selectedDish ? (
-      props.selectedDish.comments.map((c) => (
-        <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>
-      ))
+    {props ? (
+      props.comments.map((c) => <ListGroup.Item key={c.id}>{c.comment}</ListGroup.Item>)
     ) : (
       <ListGroup.Item>Click on a pasta to see the reviews!</ListGroup.Item>
     )}
